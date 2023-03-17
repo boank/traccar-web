4924ac39777baa8be490b21184b847aff5247dbd
boank <a.irwansyah76@gmail.com>
2023-03-18 00:07:58 +0700

    Update options
    Update few options for modern template

--- modern/src/setupProxy.js
+++ modern/src/setupProxy.js
@@ -3,6 +3,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  app.use(createProxyMiddleware('/api/socket', { target: `ws://${process.env.REACT_APP_URL_NAME}`, ws: true }));
-  app.use(createProxyMiddleware('/api', { target: `http://${process.env.REACT_APP_URL_NAME}` }));
+  app.use(createProxyMiddleware('/api/socket', { target: `wss://${process.env.REACT_APP_URL_NAME}`, ws: true }));
+  app.use(createProxyMiddleware('/api', { target: `https://${process.env.REACT_APP_URL_NAME}` }));
 };
